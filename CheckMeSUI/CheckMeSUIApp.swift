e5e2641aa7cfa77fa4b2212442c026597992a532
DmKeller <dmit.keller@gmail.com>
2023-06-20 23:48:28 +0300

    Initial Commit

---
+++ CheckMeSUI/CheckMeSUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CheckMeSUIApp.swift
+//  CheckMeSUI
+//
+//  Created by Келлер Дмитрий on 20.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct CheckMeSUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
